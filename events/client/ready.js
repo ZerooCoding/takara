8e81ee254fec396f6f5e22cf16d32404d90b7c88
Kommunikation <bravonikki13@gmail.com>
2020-10-15 19:55:13 +0200

    lol

---
+++ events/client/ready.js
@@ -0,0 +1,37 @@
+const config = require(`../../takara/config.json`);
+function setPresence(Client, count) {
+    const status = [
+        "on $GUILDS guilds",
+        "$USER total member",
+        "in $CHANNEL channels"
+    ];
+    const index = Math.floor(Math.random() * (status.length - 1) + 1);
+    const game = status[index]
+        .replace("$USER", count)
+        .replace("$GUILDS", Client.guilds.cache.size)
+        .replace("$CHANNEL", Client.channels.cache.size);
+    Client.user.setPresence({
+        activity: {
+            type: "STREAMING",
+            url: "https://www.twitch.tv/DieKommunikation",
+            name: `${game} | ${config.DEFAULT_PREFIX} | V:${config.VERSION}`
+        },
+        status: "online"
+    });
+}
+module.exports = async Client => {
+    let count = 0;
+    Client.guilds.cache.forEach(guild => {
+        count += guild.memberCount;
+    });
+    Client.user.setPresence({
+        activity: {
+            name: `Starting | V: ${config.VERSION}`
+        },
+        status: "dnd"
+    });
+    setInterval(() => {
+        setPresence(Client, count);
+    }, 30000);
+    console.log(Client.user.tag);
+}
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const Discord = require("discord.js");
+const fs = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 const Client = new Discord.Client();
 
-Client.on("ready", () => {
-    console.log(Client.user.tag);
+fs.readdir("./events/client/", (error, files) => {
+    if (error) console.error(error);
+    files.forEach(file => {
+        const event = require("./events/client/" + file);
+        let eventName = file.split(".")[0];
+        Client.on(eventName, event.bind(null, Client));
+        console.log("[Event:Client] " + file);
+    });
 });
 
 Client.login(process.env.TOKEN);

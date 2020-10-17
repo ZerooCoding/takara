08cf46d593d49d0563f66a4823cbd3b4d3b7aea9
Kommunikation <bravonikki13@gmail.com>
2020-10-17 16:10:22 +0200

    + Leaderboard | + You can no longer pay bots...

---
+++ commands/economy/leaderboard.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+module.exports.run = async (Client, message, args) => {
+    let money = db.all().filter(data => data.ID.startsWith(`money_`)).sort((a, b) => b.data - a.data)
+    money.length = 10;
+    var list = "";
+    for (var i in money) {
+        list += `🏅 ${money.indexOf(money[i]) + 1}. ${Client.users.cache.get(money[i].ID.split('_')[1]) ? Client.users.cache.get(money[i].ID.split('_')[1]).tag : "Unknwon#0000"} | Balance: ${money[i].data}$\n`
+    }
+    const leaderboard = new Discord.MessageEmbed()
+        .setAuthor(Client.user.tag, Client.user.avatarURL())
+        .setDescription(list)
+        .setTimestamp();
+    return message.channel.send(leaderboard);
+};
+module.exports.help = {
+    Name: "Leaderboard",
+    Aliases: ["leaderboard"],
+    Category: "Economy",
+    Permissions: ["None"],
+    Usage: ["leaderboard"],
+    Description: "Shows the top 10 richest people."
+};
---
+++ commands/economy/pay.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+module.exports.run = async (Client, message, args) => {
+    let member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
+    if (!member) return message.channel.send("I wasn't able to find any member...");
+    if (member.user.bot) return message.channel.send("This member is a bot...")
+    if (!db.get(`money_${member.id}`)) db.set(`money_${member.id}`, 0);
+    if (!Number(args[1])) return message.channel.send("You didn't input a valid number...");
+    if ((args[1] - Math.floor(args[1])) !== 0) return message.channel.send("The given number isn't valid...");
+    if (args[1] <= 0) return message.channel.send("The given number isn't valid...");
+    if (db.get(`money_${message.author.id}`) < args[1]) return message.channel.send("You don't have that much money...");
+    await db.subtract(`money_${message.author.id}`, args[1]);
+    await db.add(`money_${member.id}`, args[1]);
+    const moneyPaid = new Discord.MessageEmbed()
+        .setDescription(`${message.author} you've paid ${member} ${args[1]}${db.get(`guild.${message.guild.id}.currency`)}`)
+        .addField(`Your Balance`, `${db.get(`money_${message.author.id}`)}${db.get(`guild.${message.guild.id}.currency`)}`)
+        .addField(`${member.user.username}'s Balance`, `${db.get(`money_${member.id}`)}${db.get(`guild.${message.guild.id}.currency`)}`)
+        .setTimestamp();
+    return message.channel.send(moneyPaid);
+};
+module.exports.help = {
+    Name: "Pay",
+    Aliases: ["pay"],
+    Category: "Economy",
+    Permissions: ["None"],
+    Usage: ["pay [member] [money]"],
+    Description: "Gives someone money."
+};
